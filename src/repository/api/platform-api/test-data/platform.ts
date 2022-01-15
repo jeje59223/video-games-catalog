c13d3531fe281ad998331fafbfac6a799a745519
jeje59223 <jcnockaert@hotmail.fr>
2022-01-15 13:44:36 +0100

    add tests

---
+++ src/repository/api/platform-api/test-data/platform.ts
@@ -0,0 +1,48 @@
+{
+    id: 12;
+    name: "Neo Geo";
+    slug: "neogeo";
+    games_count: 96;
+    image_background: "https://media.rawg.io/media/screenshots/615/615351a9565d1c4ae1f39d053b1614d4.jpg";
+    image: null;
+    year_start: null;
+    year_end: null;
+    games: [
+        {
+            id: 1488,
+            slug: "metal-slug-3",
+            name: "METAL SLUG 3",
+            added: 2077
+        },
+        {
+            id: 14948,
+            slug: "metal-slug",
+            name: "METAL SLUG",
+            added: 847
+        },
+        {
+            id: 23669,
+            slug: "the-king-of-fighters-2002",
+            name: "THE KING OF FIGHTERS 2002",
+            added: 702
+        },
+        {
+            id: 24881,
+            slug: "pac-man",
+            name: "Pac-Man",
+            added: 582
+        },
+        {
+            id: 6256,
+            slug: "metal-slug-2",
+            name: "METAL SLUG 2",
+            added: 524
+        },
+        {
+            id: 3802,
+            slug: "double-dragon",
+            name: "Double Dragon",
+            added: 356
+        }
+    ]
+}
