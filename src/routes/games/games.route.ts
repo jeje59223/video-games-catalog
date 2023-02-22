e746df24791b628b53ba006a1b7572e1982fd2e2
jerome cnockaert <47606138+jeje59223@users.noreply.github.com>
2023-02-22 18:06:44 +0100

    cors

--- src/routes/games/games.route.ts
+++ src/routes/games/games.route.ts
@@ -9,6 +9,11 @@ router.get('', async (req: Request, res: Response) => {
   res.send(await GamesServices.getGames());
 });
 
+// permet d'afficher les meilleurs jeux du mois
+router.get('/best-games-of-the-month', async (req: Request, res: Response) => {
+    res.send(await GamesServices.getBestGamesOfTheMonth())
+})
+
 // permet d'afficher les screenshots
 router.get('/:gameId/screenshots', async (req: Request, res: Response) => {
     res.send(await GamesServices.getScreenshotsByGameId(req.params.gameId));
